8546b58f8e37c349ecdc0bf1e3f1cffa24b628cd
zluuba <zluyba.nikitina@gmail.com>
2023-02-21 14:34:59 +0300

    Feat: add url_checks table

--- database.sql
+++ database.sql
@@ -1,6 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
+
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
     created_at TIMESTAMP NOT NULL
 );
