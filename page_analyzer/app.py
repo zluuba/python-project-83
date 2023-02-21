8a8e7e67f04c586d09b8290beb33d25dcf4b678a
zluuba <zluyba.nikitina@gmail.com>
2023-02-21 12:02:33 +0300

    Fix: major changes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template, request, flash, redirect, get_flashed_messages, url_for
-from datetime import date
+from page_analyzer.validator import validate
+from psycopg2.extras import NamedTupleCursor
 import psycopg2
+import datetime
 import os
+import re
+
+from flask import (
+    Flask, render_template,
+    request, redirect,
+    flash, get_flashed_messages,
+    url_for
+)
+
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
-try:
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    conn = psycopg2.connect(DATABASE_URL)
-except:
-    print('Can`t establish connection to database')
-
-
-def validate(url):
-    return True
+database_url = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(database_url)
 
 
 @app.route('/')
-def index():
+def main_page():
     return render_template(
         'index.html'
     )
@@ -27,40 +30,50 @@ def index():
 @app.post('/urls')   # Button click
 def add_urls():
     url = request.form.get('url')
-    # if not url:
-    #     flash('URL обязателен', 'error')
-    #     return redirect('/')
+    errors = validate(url)
+    if errors:
+        return render_template(
+            'index.html',
+            errors=errors
+        ), 422
 
-    # url_errors = validate(url)
-    # if url_errors:
-    #     for error in url_errors:
-    #           flash(error, 'error')
-    #     return render_template(
-    #         'index.html',
-    #         search=url
-    #     ), 422
-
-    current_date = date.today()
     with conn.cursor() as cursor:
-        cursor.execute("INSERT INTO urls (name, created_at) VALUES (%(name)s, %(created_at)s)", {'name': url, 'created_at': current_date})
-        cursor.execute("SELECT id FROM urls WHERE name = %(name)s", {'name': url})
+        current_date = datetime.datetime.now()
+        cursor.execute("INSERT INTO urls (name, created_at) VALUES (%(name)s, %(created_at)s);",
+                        {'name': url, 'created_at': current_date})
+        cursor.execute("SELECT id FROM urls WHERE name = %(name)s;", {'name': url})
         url_id = cursor.fetchone()[0]
         flash('Страница успешно добавлена', 'alert-success')
         return redirect(
-            url_for('url_page',
-            id=url_id)
+            url_for('url_page', id=url_id),
         )
 
 
-@app.get('/urls/<id>')
+@app.get('/urls/<int:id>')
 def url_page(id):
+    messages = get_flashed_messages()
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute("SELECT * FROM urls WHERE id = %s;", (id,))
+        url_data = cursor.fetchone()
+
+    url, date = url_data.name, url_data.created_at.strftime('%Y-%m-%d')
+    domain = re.match(r"^[a-z]+://([^/:]+)", url).group(0)
+
     return render_template(
-        'url_page.html'
+        'url_page.html',
+        messages=messages,
+        id=id,
+        url=domain,
+        date=date
     )
 
 
 @app.route('/urls', methods=['GET'])   # Header
 def urls_list():
-    return render_template(
-        'urls.html'
-    )
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM urls ORDER BY created_at DESC;")
+        added_urls = cursor.fetchall()
+        return render_template(
+            'urls.html',
+            urls=added_urls,
+        )
