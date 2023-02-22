ae6c94cd6883dae20afb72008e7df0c41909fce6
zluuba <zluyba.nikitina@gmail.com>
2023-02-22 14:26:34 +0300

    Fix: minor changes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -116,7 +116,9 @@ def check(id):
             html = BeautifulSoup(response.content, 'html.parser')
             h1 = html.h1.string if html.h1 else None
             title = html.title.string if html.title else None
-            description = [tag.get('content') for tag in html.find_all('meta') if tag.get('name') == 'description']
+            description = [
+                tag.get('content') for tag in html.find_all('meta') if
+                tag.get('name') == 'description']
             description = description[0] if description else None
 
             current_date = datetime.datetime.now()
