991ce3602112edaa071666a44880cd95d657ce52
zluuba <zluyba.nikitina@gmail.com>
2023-02-25 10:27:16 +0300

    Feat: add length check func

--- page_analyzer/common.py
+++ page_analyzer/common.py
@@ -1,5 +1,17 @@
+from page_analyzer.validator import MAX_LENGTH
 from bs4 import BeautifulSoup
 import requests
+
+
+HIDE_CHARS = '...'
+
+
+def get_valid_length_data(data):
+    if data:
+        if len(data) > MAX_LENGTH:
+            optimal_length = MAX_LENGTH - len(HIDE_CHARS)
+            data = data[:optimal_length] + HIDE_CHARS
+    return data
 
 
 def get_html_data(url):
@@ -15,5 +27,9 @@ def get_html_data(url):
         tag.get('name') == 'description'
     ]
     description = description[0] if description else None
+
+    h1 = get_valid_length_data(h1)
+    title = get_valid_length_data(title)
+    description = get_valid_length_data(description)
 
     return status_code, h1, title, description
